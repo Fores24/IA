27081e1997f631f4cf9255b96fce7b55e5899198
Fores24 <saul.almazan@estudiante.uam>
2021-03-15 22:50:20 +0100

    s

--- P2/Soluciones.py
+++ P2/Soluciones.py
@@ -1,13 +1,8 @@
 import math
 from reversi import Reversi
-import reversi
 from heuristic import *
-from game import (
-    TwoPlayerGameState,
-)
-from tournament import (
-    StudentHeuristic,
-)
+from game import TwoPlayerGameState
+from tournament import StudentHeuristic
 
 
 class Solution1(StudentHeuristic):
@@ -30,43 +25,21 @@ class Solution3(StudentHeuristic):
   def evaluation_function(self, state: TwoPlayerGameState) -> float:
     if not isinstance(state.game, Reversi):
       return 0
-    if isinstance(state.board, dict):
-      board = state.board
-    else:
-      board = reversi.from_array_to_dictionary_board(state.board)
-    if state.end_of_game:
-      scores = state.scores
-      result = scores[0] - scores[1]
-    else:
-      game = state.game
-      corners = [board.get((1, 1)), board.get((1, game.height)), board.get((game.width, 1)),
-                 board.get((game.width, game.height))]
-      x_squares = [board.get((2, 2)), board.get((2, game.height - 1)), board.get((game.width - 1, 2)),
-                   board.get((game.width - 1, game.height - 1))]
-      corner_diff = 0
-      for i in range(4):
-        if corners[i] == game.player1.label:
-          corner_diff += 4
-        elif corners[i] == game.player2.label:
-          corner_diff -= 4
-        else:
-          if x_squares[i] == game.player1.label:
-            corner_diff -= 1
-          elif x_squares[i] == game.player2.label:
-            corner_diff += 1
+    print("hola")
+    corners = [[1, 1], [1, 8], [8, 1], [8, 8]]
+    minDist = 10
+    game = state.game
+    moves = game.generate_successors(state)
+    print(moves)
+    for move in moves:
+      for corner in corners:
+        distX = abs(corner[0] - move.x)
+        distY = abs(corner[1] - move.y)
+        dist = math.sqrt(distX * distX + distY * distY)
+        if dist < minDist:
+          minDist = dist
 
-      mobility = len(game.generate_successors(state))
-
-      result = 4 * corner_diff - mobility
-
-    if state.is_player_max(state.player1):
-      return result
-    elif state.is_player_max(state.player2):
-      return - result
-    else:
-      raise ValueError('Player MAX not defined')
-
-
+    return minDist
 
 
 
