eaf98b0fcb08f59b93d1188b402edd2e88f3f91b
lilg8b <60981604+XXXPASION@users.noreply.github.com>
2021-04-20 15:33:56 +0200

    Update practica.pl

--- practica.pl
+++ practica.pl
@@ -1,19 +1,13 @@
 %Practicas 3 IA - UAM --> PROLOG
-%By: Guillermo Hoyo Bravo, Saul Almanza ---
+%By: Guillermo Hoyo Bravo, Saul Almanza del Pie
 %
-
-%APARTADO DE OBJETOS
-%
-
-% Numero 2
-numero(2).
 
 % --------------------------FUNCIONES--GLOBALES-------------------------------%
-%FUNCIONES COPIADAS DE PÁGINA - https://www.ic.unicamp.br/~meidanis/courses/mc336/2009s2/prolog/problemas/
+%FUNCIONES COPIADAS DE PÃGINA - https://www.ic.unicamp.br/~meidanis/courses/mc336/2009s2/prolog/problemas/
 
-% TAMAÑO DE UNA LISTA == FUNCION length(L,tam).
+% TAMAÃ‘O DE UNA LISTA == FUNCION length(L,tam).
 % L es una Lista
-% N es el número de elementos de la lista
+% N es el nÃºmero de elementos de la lista
 tam([],0).
 tam([_|L],N) :- tam(L,N), N is N + 1.
 
@@ -25,7 +19,7 @@ penultimo(X,[_,Y|Ys]) :- penultimo(X,[Y|Ys]).
 % SACAR ELEMENTO K DE UNA LISTA
 % X elemento a encontrar
 % [X|L] = LISTA [X = Cabeza || L = Cola]
-% K = Posición del elemento
+% K = PosiciÃ³n del elemento
 element_at(X,[X|_],1).
 element_at(X,[_|L],K) :- K > 1, K1 is K - 1, element_at(X,L,K1).
 
@@ -34,16 +28,29 @@ element_at(X,[_|L],K) :- K > 1, K1 is K - 1, element_at(X,L,K1).
 no_pertenece(X,[]).
 no_pertenece(X,[Y|Ys]):- X\=Y, no_pertenece(X, Ys).
 
-% --------------------------------EJERCICIOS------------------------------------%
+write_log(S) :- open('error_logs.txt', append, Out), write(Out, S), write(Out, '\n'), close(Out).
+
+/***************
+* EJERCICIO 1. sum_pot_prod/4
+*
+*	ENTRADA:
+*		X: Vector de entrada de numeros de valor real.
+*		Y: Vector de entrada de numeros de valor real.
+*		Potencia: Numero de valor entero, potencia.
+*	SALIDA:
+*		Resultado: Numero de valor real resultado de la operacion sum_pot_prod. 
+*
+****************/
+sum_pot_prod(X, Y, Potencia, Resultado) :- print('Error. Este ejercicio no esta implementado todavia.'), !, fail.
 
 %EJERICIO 1 - Producto, Potencua, Sumatorio --> sum_pot_prod/4
 %sum_pot_prod(X, Y, P, R)
-%    X = Lista Numérica
-%    Y = Lista Numérica
+%    X = Lista NumÃ©rica
+%    Y = Lista NumÃ©rica
 %    P = Potencia
 %    R = Resultado
 %
-%    Enunciado: Multiplicar terminos de cada lista, elevar esos número a
+%    Enunciado: Multiplicar terminos de cada lista, elevar esos nÃºmero a
 %    P y sumarlos para obtener R.
 %
 
@@ -61,8 +68,8 @@ eval([X|Xs], [Y|Ys], P, C):- sum_pot_prod(Xs, Ys, P, C1), C is C1 + (X*Y)^P.
 %EJERCICIO 2 - Segundo y Penultimo de la Lista --> segundo_penultimo/3
 %segundo_penultimo(L,X,Y)
 %    L es la Lista para extrar los elementos
-%    X es el segundo número de la lista
-%    Y es el penúltimo número de la lista
+%    X es el segundo nÃºmero de la lista
+%    Y es el penÃºltimo nÃºmero de la lista
 %
 
 segundo_penultimo(L,X,Y):- (tam2(L,T),  (T < 2), write("ERROR 2.1 Longitud"), fail); (segundo(X,L), penultimo(Y,L)).
@@ -93,8 +100,8 @@ sublista(L, Menor, Mayor, E, Sublista).
 %Espacio_lineal(Mayor, Menor, N, Rejilla)
 %    Menor, Mayor = Indices superiores e inferiores donde se despliega
 %    el espacio lineal
-%    N = Numero de valores en las regíllas
-%    Rejilla = Lista de números del Espacio Lineal
+%    N = Numero de valores en las regÃ­llas
+%    Rejilla = Lista de nÃºmeros del Espacio Lineal
 %
 
 
@@ -103,17 +110,17 @@ sublista(L, Menor, Mayor, E, Sublista).
 
 
 
-%EJERCICIO 5 - Normalizarción de listas --> normalizar/2
+%EJERCICIO 5 - NormalizarciÃ³n de listas --> normalizar/2
 %Normalizar(Distribucion_sin_Normalizar, Distribucion_Nomalizada).
-%    Distribucion_sin_Normalizar = Lista de Número a Normalizar
-%    Distribucion_Normalizada = Lista de Números Normalziados
+%    Distribucion_sin_Normalizar = Lista de NÃºmero a Normalizar
+%    Distribucion_Normalizada = Lista de NÃºmeros Normalziados
 %
 %    No pueden ser negativos
 %
 
 %EJERCICIO 6 - Divergencia --> divergencia_k1/3
 %Divergencia_k1(D1,D2,KL)
-%    D1 = Lista de números
-%    D2 = Lista de números
+%    D1 = Lista de nÃºmeros
+%    D2 = Lista de nÃºmeros
 %    KL =
 %
